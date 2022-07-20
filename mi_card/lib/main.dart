09257f849aebd4f8510b2b9f64e6e99555187796
Ayma-n <aymanbt14@gmail.com>
2022-07-20 14:45:49 +0100

    completed mi_card layout challenge successfully :)

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LayoutChallenge());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +13,41 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
+      ),
+    );
+  }
+}
+
+class LayoutChallenge extends StatelessWidget {
+  const LayoutChallenge({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(color: Colors.yellow, width: 100.0, height: 100.0),
+                  Container(color: Colors.green, width: 100.0, height: 100.0),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
