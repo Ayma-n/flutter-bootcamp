694a5ed87baf5c4dbf1e4823f51491e95a5f83fa
Ayma-n <aymanbt14@gmail.com>
2022-08-05 17:33:29 +0300

    completed basic scaffold layout for magic 8 ball

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: new BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        backgroundColor: Colors.blue,
+        body: Container(),
+      ),
+    );
+  }
+}
