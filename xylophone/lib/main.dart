aca596cddf8695ddc3fb274ee56d153806908eec
Ayma-n <aymanbt14@gmail.com>
2022-08-06 16:43:59 +0300

    added audioplayers package and poc code

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +10,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              child: Text("Click Me!"),
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('assets/note1.wav'));
+              },
+            ),
+          ),
         ),
       ),
     );
